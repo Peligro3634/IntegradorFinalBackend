e0dd4f22ffea7dbd5ac382bab01fc92a1a320565
Peligro Dev <111589151+Peligro3634@users.noreply.github.com>
2023-05-15 21:48:37 -0300

    condocker

--- src/main/java/com/repoandresc/repo/RepoApplication.java
+++ src/main/java/com/repoandresc/repo/RepoApplication.java
@@ -2,7 +2,7 @@ package com.repoandresc.repo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//argprograma
+//argprograma final
         
         
 @SpringBootApplication
