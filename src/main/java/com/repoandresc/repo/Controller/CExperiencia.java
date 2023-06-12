1571c71beab9dfad2964404d9314c57caf662ab9
Peligro Dev <111589151+Peligro3634@users.noreply.github.com>
2023-06-12 17:49:32 -0300

    correccion de direccion web

--- src/main/java/com/repoandresc/repo/Controller/CExperiencia.java
+++ src/main/java/com/repoandresc/repo/Controller/CExperiencia.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = {"http://localhost:4200https://integradorfinalandresceb.web.app"})
+@CrossOrigin(origins = {"https://integradorfinalandresceb.web.app"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
