d2ca0f13fc52fde090b77981b86c0bee86f63554
Peligro Dev <111589151+Peligro3634@users.noreply.github.com>
2023-06-13 21:36:04 -0300

    Update AuthController.java

--- src/main/java/com/repoandresc/repo/Security/Controller/AuthController.java
+++ src/main/java/com/repoandresc/repo/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins={"https://integradorfinalandresceb.web.app/","http://localhost:4200"})
+@CrossOrigin(origins={"https://integrador-argentina-programa.web.app","http://localhost:4200"})
 
 
 public class AuthController {
